3e652950747295bd5cd82756a6d2f2efb7f543d9
Mochammad Qemal Firza <kemalfirza@gmail.com>
2023-02-15 17:39:56 +0700

    in out channel

--- goroutine_test.go
+++ goroutine_test.go
@@ -14,21 +14,26 @@ func RunMukbang() {
 	fmt.Println("Mari kita mukbang")
 }
 
-func ResponseChannel(channel chan string) {
-	time.Sleep(2 * time.Second)
+func GiveChannelOnlyIn(channel chan<- string) {
+	time.Sleep(3 * time.Second)
 	channel <- "Hayuk Mabar Bro"
+}
+
+func ReceiveChannelOnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
 }
 
 func TestGoroutine(t *testing.T) {
 	channel := make(chan string)
 	defer close(channel)
-	go ResponseChannel(channel)
-	data := <-channel
+
 	for i := 0; i < 3; i++ {
+		go GiveChannelOnlyIn(channel)
+		go ReceiveChannelOnlyOut(channel)
 		go RunHelloGaes(i)
 		go RunMukbang()
-		fmt.Println(data)
 		fmt.Println("eits kenapa tuch")
 	}
-	time.Sleep(1 * time.Second)
+	time.Sleep(5 * time.Second)
 }
